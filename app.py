08ae172a70113e110b716b064d0a24359b53a959
JoaoVictorCRP <joaovictorxl120@gmail.com>
2024-05-18 17:46:16 -0300

    formulario de agendamento

--- app.py
+++ app.py
@@ -7,5 +7,9 @@ app.config['DEBUG'] = True
 def home():
     return render_template('index.html')
 
+@app.route('/agendar-consulta')
+def form_consulta():
+    return render_template('form-consulta.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
