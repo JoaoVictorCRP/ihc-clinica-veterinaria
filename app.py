35865e9785689f912060940562007a7619d3b1a6
JoaoVictorCRP <joaovictorxl120@gmail.com>
2024-05-23 17:41:14 -0300

    fixme: sidebar

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -13,8 +13,17 @@ def sobre():
     return render_template('sobre.html')
 
 # Versão Secretária/Veterinário
-@app.route('/adm-login')
+@app.route('/adm-login', methods =['POST',  'GET'])
 def adm():
+
+    if request.method == 'POST':
+        user = request.form['username']        
+        password = request.form['password']
+
+        print(user, password)
+        if user and password: # Obviamente, teríamos uma validação no BD aqui, mas como é um protótipo, isso não é necessário.
+            return render_template ('adm-painel.html', user=user)
+
     return render_template('adm-login.html')
 
 if __name__ == '__main__':
