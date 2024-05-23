a40a4475366cc4a4066e38bd51c3cab855b4cf17
JoaoVictorCRP <joaovictorxl120@gmail.com>
2024-05-22 22:22:00 -0300

    login painel administrativo

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
+# Versão cliente
 @app.route('/')
 def home():
     return render_template('index.html')
@@ -10,6 +11,11 @@ def home():
 @app.route('/sobre-nos')
 def sobre():
     return render_template('sobre.html')
+
+# Versão Secretária/Veterinário
+@app.route('/adm-login')
+def adm():
+    return render_template('adm-login.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
